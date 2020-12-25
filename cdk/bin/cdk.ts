c3915d9325d3b644deafd32719af0d0d7e9929e8
Edward Cho <eunseocho@gmail.com>
2020-12-24 16:07:35 -0800

    make the pipeline synth properly

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,23 +2,39 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import * as Route53 from 'aws-sdk/clients/route53';
+import * as SecretsManager from 'aws-sdk/clients/secretsmanager';
 
 import { FrontendStack } from '../lib/frontend-stack';
+import { CdkStack } from '../lib/cdk-stack';
 
 async function listHostedZones(): Promise<Route53.ListHostedZonesResponse> {
   const route53 = new Route53();
   return route53.listHostedZones().promise();
 }
 
+async function describeSecret(secretId: string): Promise<SecretsManager.DescribeSecretResponse> {
+  const sm = new SecretsManager();
+  const request: SecretsManager.DescribeSecretRequest = {
+    SecretId: secretId,
+  };
+
+  return sm.describeSecret(request).promise();
+}
+
 (async function () {
   const domainName = 'eddiecho.io';
   const hostedZones = await listHostedZones();
   const hostedZone = hostedZones.HostedZones.filter(zone => zone.Name.includes(domainName))[0];
+  const githubSecret = await describeSecret('GithubPersonalAccessToken');
 
   const app = new cdk.App();
   new FrontendStack(app, 'PersonalSiteFrontend', {
     hostedZoneId: hostedZone.Id,
     hostedZoneName: hostedZone.Name,
     domainName: domainName,
+  });
+
+  new CdkStack(app, 'PersonalStackCicd', {
+    secretArn: githubSecret.ARN as string,
   });
 })();
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,8 @@
+import * as CodeBuild from '@aws-cdk/aws-codebuild';
 import * as CodePipeline from '@aws-cdk/aws-codepipeline';
 import * as CodePipelineActions from '@aws-cdk/aws-codepipeline-actions';
+import * as Iam from '@aws-cdk/aws-iam';
+import * as S3 from '@aws-cdk/aws-s3';
 import * as SecretsManager from '@aws-cdk/aws-secretsmanager';
 import * as Cdk from '@aws-cdk/core';
 
@@ -14,11 +17,50 @@ export class CdkStack extends Cdk.Stack {
     super(scope, id, props);
     this.props = props;
 
+    const artifactBucket = new S3.Bucket(this, 'PersonalSitePipelineArtifactBucket');
+
     new CodePipeline.Pipeline(this, 'PersonalSitePipeline', {
+      artifactBucket: artifactBucket,
       restartExecutionOnUpdate: true,
       stages: this.renderPipelineStages(),
     });
   }
+
+  private renderCompileCdkProject = (): CodeBuild.PipelineProject => {
+    const project = new CodeBuild.PipelineProject(this, 'CompileCdkProject', {
+      buildSpec: CodeBuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: ['chmod +x cdk/bin/*'],
+          },
+          build: {
+            commands: ['./cdk/bin/compile_cdk.sh'],
+          },
+        },
+        artifacts: {
+          'base-directory': 'cdk',
+          files: ['cdk.out/**/*'],
+        },
+      }),
+      environment: {
+        buildImage: CodeBuild.LinuxBuildImage.STANDARD_4_0,
+      },
+    });
+
+    const secretPolicy = new Iam.PolicyStatement();
+    secretPolicy.addActions('secretsmanager:DescribeSecret');
+    secretPolicy.addResources(this.props.secretArn);
+
+    const networkPolicy = new Iam.PolicyStatement();
+    networkPolicy.addActions('route53:ListHostedZones');
+    networkPolicy.addResources('*');
+
+    project.addToRolePolicy(secretPolicy);
+    project.addToRolePolicy(networkPolicy);
+
+    return project;
+  };
 
   private renderPipelineStages = (): CodePipeline.StageProps[] => {
     const sourceOutput = new CodePipeline.Artifact();
@@ -27,6 +69,8 @@ export class CdkStack extends Cdk.Stack {
       'GithubSecret',
       this.props.secretArn
     ).secretValueFromJson('OAuth');
+
+    const cdkOutput = new CodePipeline.Artifact();
 
     return [
       {
@@ -39,6 +83,17 @@ export class CdkStack extends Cdk.Stack {
             owner: 'eddiecho',
             repo: 'personalblog',
             branch: 'mainline',
+          }),
+        ],
+      },
+      {
+        stageName: 'Build',
+        actions: [
+          new CodePipelineActions.CodeBuildAction({
+            actionName: 'CompileCDK',
+            input: sourceOutput,
+            outputs: [cdkOutput],
+            project: this.renderCompileCdkProject(),
           }),
         ],
       },
